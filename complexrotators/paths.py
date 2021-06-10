793fb59abf68f3fa9beaa45060028d51a5979560
lgbouma <bouma.luke@gmail.com>
2021-06-10 12:54:49 -0400

    add multicolor phase plots

--- complexrotators/paths.py
+++ complexrotators/paths.py
@@ -2,8 +2,8 @@ import os, socket
 from complexrotators import __path__
 
 DATADIR = os.path.join(os.path.dirname(__path__[0]), 'data')
+PHOTDIR = os.path.join(DATADIR, 'photometry', 'ground')
 RESULTSDIR = os.path.join(os.path.dirname(__path__[0]), 'results')
-PHOTDIR = os.path.join(DATADIR, 'photometry')
 
 LOCALDIR = os.path.join(os.path.expanduser('~'), 'local', 'complexrotators')
 if not os.path.exists(LOCALDIR):
--- complexrotators/plotting.py
+++ complexrotators/plotting.py
@@ -4,12 +4,14 @@ Contents:
 
     plot_river
     plot_phase
+    plot_multicolor_phase
 """
 import os, corner, pickle
 from glob import glob
 from datetime import datetime
 import numpy as np, matplotlib.pyplot as plt, pandas as pd, pymc3 as pm
 from numpy import array as nparr
+from collections import OrderedDict
 
 import matplotlib as mpl
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -28,6 +30,8 @@ from astrobase.lcmath import (
     phase_magseries, phase_bin_magseries, sigclip_magseries,
     find_lc_timegroups, phase_magseries_with_errs, time_bin_magseries
 )
+
+from complexrotators.paths import DATADIR, PHOTDIR
 
 def plot_TEMPLATE(outdir):
 
@@ -150,9 +154,13 @@ def plot_river(time, flux, period, outdir, titlestr=None, cmap='Blues_r',
 
 
 def plot_phase(outdir):
+    # currently hard-coded for TIC 262400835
 
-    # get data (20sec, hardcopy for TIC 262400835)
-    df = pd.read_csv('/Users/luke/Dropbox/proj/complexrotators/results/river/tic_262400835/HARDCOPY_spoc_tess_lightcurve_median_PDCSAP_FLUX_allsector_sigclipped.csv')
+    # get data (20 sec)
+    df = pd.read_csv(
+        '/Users/luke/Dropbox/proj/complexrotators/results/river/tic_262400835/'
+        'HARDCOPY_spoc_tess_lightcurve_median_PDCSAP_FLUX_allsector_sigclipped.csv'
+    )
     time, flux = np.array(df.time), np.array(df.flux)
 
     t0 = np.nanmin(time) + 0.5/24
@@ -165,23 +173,172 @@ def plot_phase(outdir):
     plot_phased_light_curve(time, flux, t0, period, outpath, titlestr=titlestr)
 
 
-def plot_multicolor_phase(outdir):
-    raise NotImplementedError
+def _get_all_tic262400835_data():
+    # get median-filtered tess 20 second data. (4.5day blended rotation
+    # removed).
+    df0 = pd.read_csv(
+        '/Users/luke/Dropbox/proj/complexrotators/results/river/tic_262400835/'
+        'HARDCOPY_spoc_tess_lightcurve_median_PDCSAP_FLUX_allsector_sigclipped.csv'
+    )
+    time, flux = np.array(df0.time), np.array(df0.flux)
+    tess_texp = np.nanmedian(np.diff(time))
 
-    # get data (20sec, hardcopy for TIC 262400835)
-    df0 = pd.read_csv('/Users/luke/Dropbox/proj/complexrotators/results/river/tic_262400835/HARDCOPY_spoc_tess_lightcurve_median_PDCSAP_FLUX_allsector_sigclipped.csv')
-    time, flux = np.array(df.time), np.array(df.flux)
+    datasets = OrderedDict()
+    datasets['tess20sec'] = [time, flux, None, tess_texp]
 
-    t0 = np.nanmin(time) + 0.5/24
+    # get MUSCAT1 data
+    m1_files = glob(
+        os.path.join(PHOTDIR, 'TIC262400835_*muscat_*.csv')
+    )
+    for m1_file in m1_files:
+        color = os.path.basename(m1_file).split("_")[3]
+        timestamp = os.path.basename(m1_file).split("_")[1]
+        assert color in ['g','r','z']
+        datasetname = 'M1_'+color+"_"+timestamp
+        _df = pd.read_csv(m1_file)
+        time, flux, err = nparr(_df.BJD_TDB), nparr(_df.Flux), nparr(_df.Err)
+        texp = np.nanmedian(np.diff(time))
+        datasets[datasetname] = [time, flux, err, texp]
+
+    # get MUSCAT2 data
+    m2_files = glob(
+        os.path.join(PHOTDIR, 'tic262400835_*achromatic*.fits')
+    )
+    for m2_file in m2_files:
+        timestamp = os.path.basename(m2_file).split("_")[1]
+        hl = fits.open(m2_file)
+        # two transits...
+        for color in ['g','r','z_s']:
+            fitskey = 'flux_'+color
+            datasetname = 'M2_'+color.replace('_s','')+"_"+timestamp
+            time, flux, err = (
+                nparr(hl[fitskey].data['time_bjd']),
+                nparr(hl[fitskey].data['flux']),
+                None
+            )
+            texp = np.nanmedian(np.diff(time))
+            datasets[datasetname] = [time, flux, err, texp]
+
+    return datasets
+
+
+def plot_multicolor_phase(outdir, BINMS=2):
+    # currently hard-coded for TIC 262400835
+
+    #keys: tess20sec, M1_g|r|z_201216, M2_g|r|z_201213|201215
+    datasets = _get_all_tic262400835_data()
+
+    t0 = np.nanmin(datasets['tess20sec'][0]) + 0.5/24
     period = 0.29822 # manually tuned...
 
-    outpath = os.path.join(outdir, 'TIC262400835_phase.png')
+    # vision:
+    # TESS phase-fold (binned and raw)
+    # g-r phase-fold
+    # g-z phase-fold
+    # ... but first, just look at the data.
 
-    titlestr = 'TIC262400835 S32 20sec'
+    # initialize
+    outpath = os.path.join(outdir, 'TIC262400835_multicolor_phase_stacked.png')
+    time, flux = datasets['tess20sec'][0], datasets['tess20sec'][1]
+    fig, ax = plot_phased_light_curve(time, flux, t0, period, None,
+                                      savethefigure=False, figsize=(4,8),
+                                      showtext=False)
+    keylist = [
+        'M2_g_201213', 'M2_r_201213', 'M2_z_201213',
+        'M2_g_201215', 'M2_r_201215', 'M2_z_201215',
+        'M1_g_201216', 'M1_r_201216', 'M1_z_201216'
+    ]
+    y_offset = -10
+    BPTOCOLORDICT = {
+        'g': 'b', 'r': 'g', 'z': 'r'
+    }
+    ix = 0
+    orb_bds = {}
+    for k in keylist:
+        bandpass = k.split("_")[1]
+        color = BPTOCOLORDICT[bandpass]
+        time, flux = datasets[k][0], datasets[k][1]
+        x,y = time, flux-np.nanmean(flux)
+        _pd = phase_magseries(x, y, period, t0, wrap=True, sort=True)
+        x_fold, y = _pd['phase'], _pd['mags']
+        ax.scatter(x_fold, 1e2*(y)+y_offset, color=color, label="data", marker='.',
+                   s=1, rasterized=True, alpha=0.3, linewidths=0)
 
-    plot_phased_light_curve(time, flux, t0, period, outpath, titlestr=titlestr)
+        bs_min = 15
+        bs_days = (bs_min / (60*24))
+        orb_bd = phase_bin_magseries(x_fold, y, binsize=bs_days, minbinelems=1)
+        ax.scatter(
+            orb_bd['binnedphases'], 1e2*(orb_bd['binnedmags'])+y_offset, color=color,
+            s=BINMS, linewidths=0, alpha=1, zorder=1002
+        )
+        orb_bds[k] = orb_bd
 
+        props = dict(boxstyle='square', facecolor='white', alpha=0.7, pad=0.15,
+                     linewidth=0)
+        txt = k
+        ax.text(.95, 2.5+np.nanmedian(1e2*(y)+y_offset), txt,
+                ha='right',va='center', color='k',
+                fontsize='xx-small', bbox=props, zorder=1003)
 
+        if ix % 3 != 2:
+            y_offset -= 5
+        else:
+            y_offset -= 10
+
+        ix += 1
+
+    savefig(fig, outpath, dpi=350)
+    plt.close('all')
+
+    # now do it in bonafide color
+    outpath = os.path.join(outdir, 'TIC262400835_multicolor_phase.png')
+
+    time, flux = datasets['tess20sec'][0], datasets['tess20sec'][1]
+    fig, ax = plot_phased_light_curve(time, flux, t0, period, None,
+                                      savethefigure=False, figsize=(4,8),
+                                      showtext=False)
+    keylist = [
+        ('M2_g_201213','M2_r_201213'),
+        ('M2_r_201213','M2_z_201213'),
+        ('M2_g_201213','M2_z_201213'),
+        ('M2_g_201215','M2_r_201215'),
+        ('M2_r_201215','M2_z_201215'),
+        ('M2_g_201215','M2_z_201215'),
+        ('M1_g_201216','M1_r_201216'),
+        ('M1_r_201216','M1_z_201216'),
+        ('M1_g_201216','M1_z_201216'),
+    ]
+    y_offset = -10
+    ix = 0
+    for b0, b1 in keylist:
+
+        c0, c1 = b0.split("_")[1], b1.split("_")[1]
+        x0, y0 = orb_bds[b0]['binnedphases'], orb_bds[b0]['binnedmags']
+        x1, y1 = orb_bds[b1]['binnedphases'], orb_bds[b1]['binnedmags']
+
+        # average of phases...
+        x = x0 + (x1-x0)/2
+        # the ~color
+        y = ( (1+y0) / (1+y1) ) - 1
+
+        ax.scatter(
+            x, 1e2*(y)+y_offset, color='k',
+            s=BINMS, linewidths=0, alpha=1, zorder=1002
+        )
+
+        props = dict(boxstyle='square', facecolor='white', alpha=0.7, pad=0.15,
+                     linewidth=0)
+        txt = b0.split("_")[0] + "_" + b0.split("_")[-1] + "_"+ c0 + "/" + c1
+        ax.text(.95, 2.5+np.nanmedian(1e2*(y)+y_offset), txt,
+                ha='right',va='center', color='k',
+                fontsize='xx-small', bbox=props, zorder=1003)
+
+        y_offset -= 6
+
+        ix += 1
+
+    savefig(fig, outpath, dpi=350)
+    plt.close('all')
 
 
 
@@ -189,16 +346,20 @@ def plot_multicolor_phase(outdir):
 
 def plot_phased_light_curve(
     time, flux, t0, period, outpath,
-    ylimd=None, binsize_minutes=2, BINMS=2, titlestr=None
+    ylimd=None, bs_min=2, BINMS=2, titlestr=None,
+    savethefigure=True, showtext=True, figsize=None
 ):
-
-    x,y = time, flux-np.nanmean(flux)
 
     # make plot
     plt.close('all')
     set_style()
 
-    fig, ax = plt.subplots(figsize=(4,3))
+    if figsize is None:
+        fig, ax = plt.subplots(figsize=(4,3))
+    else:
+        fig, ax = plt.subplots(figsize=figsize)
+
+    x,y = time, flux-np.nanmean(flux)
 
     # time units
     # x_fold = (x - t0 + 0.5 * period) % period - 0.5 * period
@@ -217,19 +378,21 @@ def plot_phased_light_curve(
     ax.scatter(x_fold, 1e2*(y), color="darkgray", label="data", marker='.',
                s=1, rasterized=True, alpha=0.3, linewidths=0)
 
-    binsize_days = (binsize_minutes / (60*24))
-    orb_bd = phase_bin_magseries(
-        x_fold, y, binsize=binsize_days, minbinelems=3
-    )
+    bs_days = (bs_min / (60*24))
+    orb_bd = phase_bin_magseries(x_fold, y, binsize=bs_days, minbinelems=3)
     ax.scatter(
         orb_bd['binnedphases'], 1e2*(orb_bd['binnedmags']), color='k',
         s=BINMS, linewidths=0,
         alpha=1, zorder=1002#, linewidths=0.2, edgecolors='white'
     )
-    txt = f'$t_0$ [BTJD]: {t0-2457000:.6f}\n$P$: {period:.6f} d'
-    ax.text(0.97,0.03,txt,
-            transform=ax.transAxes,
-            ha='right',va='bottom', color='k', fontsize='xx-small')
+    if showtext:
+        txt = f'$t_0$ [BTJD]: {t0-2457000:.6f}\n$P$: {period:.6f} d'
+        ax.text(0.97,0.03,txt,
+                transform=ax.transAxes,
+                ha='right',va='bottom', color='k', fontsize='xx-small')
+    else:
+        txt = f'$t_0$ [BTJD]: {t0-2457000:.6f}. $P$: {period:.6f} d'
+        ax.set_title(txt, fontsize='small')
 
 
     if isinstance(titlestr,str):
@@ -244,5 +407,9 @@ def plot_phased_light_curve(
 
     fig.tight_layout()
 
-    savefig(fig, outpath, dpi=350)
-    plt.close('all')
+    if savethefigure:
+        savefig(fig, outpath, dpi=350)
+        plt.close('all')
+    else:
+        return fig, ax
+
