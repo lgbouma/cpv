25b14ea4a760c58ce53440eb0a12b4116fb4eba7
lgbouma <bouma.luke@gmail.com>
2021-12-16 11:30:38 -0800

    add calc_continuum_energies

---
+++ drivers/calc_continuum_energies.py
@@ -0,0 +1,28 @@
+# hydrogen
+
+import numpy as np
+from astropy import units as u, constants as c
+
+n_1 = 3 # "Paschen" continua
+
+n_1_dict = {
+    1: 'Lyman',
+    2: 'Balmer',
+    3: 'Paschen/Bohr',
+    4: 'Brackett',
+    5: 'Pfund',
+    6: 'Humphreys',
+    7: 'seven'
+}
+
+for n_1, name in n_1_dict.items():
+    print(42*'-')
+    print(f'n_1: {n_1} ({name})')
+    for n_2 in range(n_1+1,20):
+        ΔE = (13.6*u.eV) * (1/n_1**2 - 1/n_2**2)
+        λ = c.h * c.c / ΔE
+        txt = (
+            f'{str(n_1).zfill(2)}->{str(n_2).zfill(2)}: ΔE = {ΔE.to(u.eV).value:.2f}eV, '
+            f'λ = {λ.to(u.nm).value:.1f}.nm'
+        )
+        print(txt)
