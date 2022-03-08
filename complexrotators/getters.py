5879c0948c063f23c1545401a37b8c0b39983421
lgbouma <bouma.luke@gmail.com>
2022-03-08 13:14:55 -0500

    add simpler TESS data getters

---
+++ complexrotators/getters.py
@@ -0,0 +1,80 @@
+"""
+get_2min_cadence_spoc_tess_lightcurve
+"""
+import numpy as np
+import lightkurve as lk
+
+def get_2min_cadence_spoc_tess_lightcurve(
+    ticstr: str,
+    ) -> list:
+    """
+    Args:
+        ticstr: e.g., 'TIC 441420236'.  will be passed to lightkurve and used
+        in naming plots.
+
+    Returns:
+        list containing individual sector-by-sector light curves.  If none are
+        found, an empty list is returned.
+    """
+
+    # get the light curve
+    lcc = lk.search_lightcurve(ticstr).download_all()
+
+    if lcc is None:
+        return []
+
+    # select only the two-minute cadence SPOC-reduced data; convert to a list.
+    # note that this conversion approach works for any LightCurveCollection
+    # returned by lightkurve -- no need to hand-pick the right ones.  the exact
+    # condition below says "if the interval is between 119 and 121 seconds,
+    # take it".
+    lc_list = [_l for _l in lcc
+         if
+         _l.meta['ORIGIN']=='NASA/Ames'
+         and
+         np.isclose(
+             120,
+             np.nanmedian(np.diff(_l.remove_outliers().time.value))*24*60*60,
+             atol=1
+         )
+    ]
+
+    return lc_list
+
+
+def get_20sec_cadence_spoc_tess_lightcurve(
+    ticstr: str,
+    ) -> list:
+    """
+    Args:
+        ticstr: e.g., 'TIC 441420236'.  will be passed to lightkurve and used
+        in naming plots.
+
+    Returns:
+        list containing individual sector-by-sector light curves.  If none are
+        found, an empty list is returned.
+    """
+
+    # get the light curve
+    lcc = lk.search_lightcurve(ticstr).download_all()
+
+    if lcc is None:
+        return []
+
+    # select only the two-minute cadence SPOC-reduced data; convert to a list.
+    # note that this conversion approach works for any LightCurveCollection
+    # returned by lightkurve -- no need to hand-pick the right ones.  the exact
+    # condition below says "if the interval is between 119 and 121 seconds,
+    # take it".
+    lc_list = [_l for _l in lcc
+         if
+         _l.meta['ORIGIN']=='NASA/Ames'
+         and
+         np.isclose(
+             20,
+             np.nanmedian(np.diff(_l.remove_outliers().time.value))*24*60*60,
+             atol=1
+         )
+    ]
+
+    return lc_list
