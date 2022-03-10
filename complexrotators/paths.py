c1ba4af59eb58499eebea63495609c218a923a57
lgbouma <bouma.luke@gmail.com>
2022-03-09 21:24:04 -0800

    paths: add table dir

--- complexrotators/paths.py
+++ complexrotators/paths.py
@@ -6,6 +6,7 @@ PHOTDIR = os.path.join(DATADIR, 'photometry', 'ground')
 CSVDIR = os.path.join(DATADIR, 'photometry', 'cdips_followup_csvs')
 TARGETSDIR = os.path.join(DATADIR, 'targetlists')
 RESULTSDIR = os.path.join(os.path.dirname(__path__[0]), 'results')
+TABLEDIR = os.path.join(RESULTSDIR, 'tables')
 
 LOCALDIR = os.path.join(os.path.expanduser('~'), 'local', 'complexrotators')
 if not os.path.exists(LOCALDIR):
