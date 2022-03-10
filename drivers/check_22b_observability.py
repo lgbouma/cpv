3c898e202397f5eeeaf2339c9ec479d08796b817
lgbouma <bouma.luke@gmail.com>
2022-03-10 08:31:07 -0800

    observability driver: add 22B constraints

--- drivers/check_22b_observability.py
+++ drivers/check_22b_observability.py
@@ -3,11 +3,11 @@ Get observing diagnostics (brightness, period, dip depth). Assumes
 "plot_22b_phases" has been run, and uses those cached products.
 """
 import os
-import pandas as pd
+import pandas as pd, numpy as np
 from glob import glob
 from complexrotators.observability import (
-    get_gaia_rows, get_tess_stats, check_astroplan_observability,
-    check_tesspoint, merge_to_obsevability_table
+    get_gaia_rows, get_tess_stats, check_astroplan_months_observable,
+    check_tesspoint, merge_to_observability_table
 )
 from complexrotators.paths import RESULTSDIR, TARGETSDIR, TABLEDIR
 from cdips.utils.catalogs import get_tic_star_information
@@ -30,11 +30,40 @@ for ticid in df.ticid.astype(str):
         r_tesspoint = check_tesspoint(
             float(r_tic8['ra']), float(r_tic8['dec']), ticid
         )
-        r_astroplan = check_astroplan_observability(ticid)
-        import IPython; IPython.embed()
-        assert 0
-        merge_to_obsevability_table(
-            csvpath, r_gaia, r_tesspoint, r_astroplan, r_tesspoint
+        r_astroplan = check_astroplan_months_observable(
+            f'TIC_{ticid}', float(r_tic8['ra']), float(r_tic8['dec']),
         )
+
+        #
+        # is object visible from Keck for at least two months during the
+        # semester?
+        #
+        # 22B: 2022/08/01 through... 2023/01/31
+        okmonths = np.array([8,9,10,11,12,1])
+        monthsobservable = np.array(
+            r_astroplan['bestmonths'][0].split(',')
+        ).astype(int)
+
+        is_keck_visible = len(np.intersect1d(okmonths, monthsobservable)) >= 2
+        r_astroplan['is_22b_keck_visible'] = is_keck_visible
+
+        #
+        # is TESS looking at the object?
+        # S55: starts 08/05/22
+        # S60: ends 01/18/23
+        #
+
+        oktessectors = np.array([55,56,57,58,59,60])
+        sectorsobservable = np.array(
+            r_tesspoint['sector'][0].split(',')
+        ).astype(int)
+
+        is_tess_visible = len(np.intersect1d(oktessectors, sectorsobservable)) >= 1
+        r_tesspoint['is_22b_tess_visible'] = is_tess_visible
+
+        merge_to_observability_table(
+            csvpath, r_gaia, r_tess, r_tic8, r_tesspoint, r_astroplan
+        )
+
     else:
         print(f'Found {csvpath}')
