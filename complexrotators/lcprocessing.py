40ff9fca7719b6d2efc2581bd85ba47aa4077102
lgbouma <bouma.luke@gmail.com>
2023-02-03 15:09:11 -0800

    cpv_periodsearch: add PDM option

--- complexrotators/lcprocessing.py
+++ complexrotators/lcprocessing.py
@@ -1,6 +1,6 @@
 """
 Contents:
-    cr_periodsearch
+    | cpv_periodsearch
 """
 import numpy as np, pandas as pd
 from numpy import array as nparr
@@ -16,7 +16,8 @@ from astrobase.lcmath import phase_magseries, phase_bin_magseries
 
 nworkers = multiprocessing.cpu_count()
 
-def cr_periodsearch(times, fluxs, starid, outdir, t0=None):
+def cpv_periodsearch(times, fluxs, starid, outdir, t0=None,
+                    periodogram_method="pdm"):
     """
     Given time and flux, run a period-search for objects expected to be complex
     rotators.
@@ -24,19 +25,43 @@ def cr_periodsearch(times, fluxs, starid, outdir, t0=None):
     A few plots and pickle files will be written to `outdir` using the `starid`
     string.
 
-    t0:
-        - None defaults to 1618.
-        - "binmin" defaults to phase-folding, and taking the arg-minimum
-        - Any int or float will be passed as the manual phase.
+    Args:
 
-    A dictionary of the results is returned, containing:
-        'lsp':lsp, 'fine_lsp':fine_lsp, 'times':times, 'fluxs':fluxs,
-        'period':fine_lsp['bestperiod'], 't0': 1618, 'outdir':outdir
+        times (np.ndarray):
+            Array of times.
+
+        fluxs (np.ndarray):
+            Array of fluxes.
+
+        starid (str):
+            Identifier used for cacheing.
+
+        outdir (str):
+            Path used for cacheing.
+
+        t0 (None, str, int, or float):
+            Epoch at which to phase.  None defaults to t0=1618.  Giving the
+            string "binmin" defaults to phase-folding, and taking the
+            arg-minimum.  Any int or float will be passed as the manual phase.
+
+        periodogram_method (str):
+            "pdm" (phase dispersion minimization) or "ls" (lomb-scargle).
+
+    Returns:
+
+        dict : results
+
+            A dictionary of the results, containing:
+                'lsp':periodogram results, 'fine_lsp':fine periodogram results,
+                'times':times, 'fluxs':fluxs, 'period':fine_lsp['bestperiod'],
+                't0': t0, 'outdir':outdir, 'periodogram_method': ...
+            Note that just because the keys are "lsp", the actual method being
+            used depends on periodogram_method
     """
 
     assert isinstance(starid, str)
 
-    pklpath = os.path.join(outdir, f"{starid}_cr_periodsearch.pkl")
+    pklpath = os.path.join(outdir, f"{starid}_cpv_periodsearch.pkl")
     if os.path.exists(pklpath):
         print(f"Found {pklpath}, loading and continuing.")
         with open(pklpath, 'rb') as f:
@@ -44,26 +69,47 @@ def cr_periodsearch(times, fluxs, starid, outdir, t0=None):
         return d
 
     sep = 1
+    # eg., a single TESS sector at 2-minute cadence has 2e4 points.  this cuts
+    # it down for period-search purposes to 4e3, which helps the runtime!
     if len(times) > 1e4:
-        sep = 10
+        sep = 5
+    # eg., a single TESS sector at 2-minute cadence has 1.2e5 points.
     if len(times) > 1e5:
-        sep = 100
+        sep = 50
 
-    startp, endp = 0.1, 5
+    startp, endp = 0.05, 5
+
+    # for the fine-tuning
     delta_P = 0.2
-    stepsize = 1e-5 # for the fine-tuning
+    stepsize = 1e-5
 
-    lsp = periodbase.pgen_lsp(
-        times[::sep], fluxs[::sep], fluxs[::sep]*1e-4, magsarefluxes=True,
-        startp=startp, endp=endp, autofreq=True, sigclip=5.0
-    )
+    if periodogram_method == 'ls':
 
-    fine_lsp = periodbase.pgen_lsp(
-        times[::sep], fluxs[::sep], fluxs[::sep]*1e-4, magsarefluxes=True,
-        startp=lsp['bestperiod']-delta_P*lsp['bestperiod'],
-        endp=lsp['bestperiod']+delta_P*lsp['bestperiod'],
-        autofreq=False, sigclip=5.0, stepsize=stepsize
-    )
+        lsp = periodbase.pgen_lsp(
+            times[::sep], fluxs[::sep], fluxs[::sep]*1e-4, magsarefluxes=True,
+            startp=startp, endp=endp, autofreq=True, sigclip=5.0
+        )
+
+        fine_lsp = periodbase.pgen_lsp(
+            times[::sep], fluxs[::sep], fluxs[::sep]*1e-4, magsarefluxes=True,
+            startp=lsp['bestperiod']-delta_P*lsp['bestperiod'],
+            endp=lsp['bestperiod']+delta_P*lsp['bestperiod'],
+            autofreq=False, sigclip=5.0, stepsize=stepsize
+        )
+
+    elif periodogram_method == 'pdm':
+
+        lsp = periodbase.stellingwerf_pdm(
+            times[::sep], fluxs[::sep], fluxs[::sep]*1e-4, magsarefluxes=True,
+            startp=startp, endp=endp, autofreq=True, sigclip=5.0
+        )
+
+        fine_lsp = periodbase.stellingwerf_pdm(
+            times[::sep], fluxs[::sep], fluxs[::sep]*1e-4, magsarefluxes=True,
+            startp=lsp['bestperiod']-delta_P*lsp['bestperiod'],
+            endp=lsp['bestperiod']+delta_P*lsp['bestperiod'],
+            autofreq=False, sigclip=5.0, stepsize=stepsize
+        )
 
     print(42*'.')
     print(f"Standard autofreq period: {lsp['bestperiod']:.7f} d")
@@ -72,12 +118,12 @@ def cr_periodsearch(times, fluxs, starid, outdir, t0=None):
     print(42*'.')
 
     outfile = os.path.join(
-        outdir, f'{starid}_lombscargle_subset_checkplot.png'
+        outdir, f'{starid}_{periodogram_method}_subset_checkplot.png'
     )
     checkplot.checkplot_png(lsp, times, fluxs, fluxs*1e-4,
                             magsarefluxes=True, phasewrap=True,
                             phasesort=True, phasebin=0.002, minbinelems=7,
-                            plotxlim=(-0.8,0.8), plotdpi=200,
+                            plotxlim=(-0.6,0.6), plotdpi=200,
                             outfile=outfile, verbose=True)
 
     if t0 is None:
@@ -107,7 +153,8 @@ def cr_periodsearch(times, fluxs, starid, outdir, t0=None):
 
     d = {
         'lsp':lsp, 'fine_lsp':fine_lsp, 'times':times, 'fluxs':fluxs,
-        'period':fine_lsp['bestperiod'], 't0': t0, 'outdir':outdir
+        'period':fine_lsp['bestperiod'], 't0': t0, 'outdir':outdir,
+        'periodogram_method': periodogram_method
         }
 
     with open(pklpath, 'wb') as f:
