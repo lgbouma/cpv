cb80d2959e9552ba4060cd77770635d77af15470
lgbouma <bouma.luke@gmail.com>
2023-03-25 15:31:41 +0100

    ibid

--- drivers/plot_phase.py
+++ drivers/plot_phase.py
@@ -6,36 +6,27 @@ PLOTDIR = os.path.join(RESULTSDIR, 'phase')
 if not os.path.exists(PLOTDIR):
     os.mkdir(PLOTDIR)
 
-# cp.plot_phase(
-#     PLOTDIR,
-#     ticid='TIC_188109809',
-#     lc_cadences='2min_20sec',
-#     manual_period=None,
-#     ylim=None,
-#     binsize_minutes=10
-# )
+def main():
 
-# NOTE: actual options are in the plotting script here
-ticids = [
-    "177309964",
-    "177309964",
-    "425933644",
-    "425933644",
-    "206544316",
-    "224283342",
-    "146539195"
-]
+    ticids = [
+        #"224283342",
+        #"146539195"
+        "402980664"
+    ]
 
-for ticid in ticids:
+    for ticid in ticids:
 
-    outdir = os.path.join(PLOTDIR, f'TIC_{ticid}')
-    if not os.path.exists(outdir): os.mkdir(outdir)
+        outdir = os.path.join(PLOTDIR, f'TIC_{ticid}')
+        if not os.path.exists(outdir): os.mkdir(outdir)
 
-    cp.plot_phase(
-        outdir,
-        ticid=f'TIC_{ticid}',
-        lc_cadences='2min',
-        ylim=None,
-        binsize_minutes=10,
-        t0='binmin'
-    )
+        cp.plot_phase(
+            outdir,
+            ticid=f'TIC_{ticid}',
+            lc_cadences='2min',
+            ylim=None,
+            binsize_minutes=10,
+            t0='binmin'
+        )
+
+if __name__ == "__main__":
+    main()
