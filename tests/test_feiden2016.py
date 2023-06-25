344c01dbe397f778fa279069bacf2dc83117b67b
lgbouma <bouma.luke@gmail.com>
2023-06-24 19:38:26 -0700

    add feiden 2016 check

---
+++ tests/test_feiden2016.py
@@ -0,0 +1,9 @@
+import numpy as np, matplotlib.pyplot as plt
+from glob import glob
+import os
+from os.path import join
+
+from complexrotators.isochroneinterp import get_Feiden2016
+
+df = get_Feiden2016()
+# and then mess around with it!
