ce7670b2078a9af1e7dda8a655fb1253dd980812
lgbouma <bouma.luke@gmail.com>
2023-07-03 16:18:08 -0700

    clean drivers/get_selection_function_values.py

--- drivers/get_selection_function_values.py
+++ drivers/get_selection_function_values.py
@@ -11,17 +11,8 @@ def add_comma(num):
     else:
         return add_comma(num // 1000) + '{,}' + num_str[-3:]
 
-localdir = '/Users/luke/local/SPOCLC'
-csvpath = join(localdir, 'gaia_X_spoc2min_merge.csv')
-df = pd.read_csv(csvpath)
-
-sel = (
-    (df.TESSMAG < 16) & (df.bp_rp > 1.5) &
-    (df.M_G > 4) & (df.parallax > 1e3*(1/150)) &
-    (df.SECTOR <= 55)
-)
-
-sdf = df[sel]
+from getters import get_cqv_search_sample
+sdf = get_cqv_search_sample()
 
 N_1 = len(np.unique(sdf.TICID))
 N_2 = len(sdf)
