41f688e0601e4f58bef79f4deee2c97d100796e1
lgbouma <bouma.luke@gmail.com>
2023-07-13 22:47:34 -0700

    update ms

--- paper/collect.sh
+++ paper/collect.sh
@@ -6,16 +6,16 @@ cp $resultsdir/lc_mosaic/lc_mosaic_fav3.pdf f1a.pdf
 cp $addir/models.pdf f1b.pdf
 
 # fig2
-cp $resultsdir/full_lcmosaic/full_lcmosaic_showtitles_Ngoodsectors_tic8_Tmag.pdf f2.pdf
+cp $resultsdir/full_lcmosaic/full_lcmosaic_showtitles_Ngoodsectors_tic8_Tmag.pdf f3.pdf
 
 # fig3
-cp $resultsdir/beforeafter_mosaic/beforeafter_mosaic_showtitles_tic8_Tmag.pdf f3.pdf
+cp $resultsdir/beforeafter_mosaic/beforeafter_mosaic_showtitles_tic8_Tmag.pdf f4.pdf
 
 # fig4
-cp $resultsdir/tic4029_segments/tic4029_segments.pdf f4.pdf
+cp $resultsdir/tic4029_segments/tic4029_segments.pdf f5.pdf
 
 # fig5
-cp $resultsdir/phase_timegroups_mosaic/TIC_402980664/TIC_402980664_P18.5611_2min_phase_timegroups_mosaic_ymin-4.8_ymax3.pdf f5.pdf
+cp $resultsdir/phase_timegroups_mosaic/TIC_402980664/TIC_402980664_P18.5611_2min_phase_timegroups_mosaic_ymin-4.8_ymax3.pdf f6.pdf
 
 # fig6
-cp $resultsdir/catalogscatter/catalogscatter_showmaybe.pdf f6.pdf
+cp $resultsdir/catalogscatter/catalogscatter_showmaybe.pdf f7.pdf
