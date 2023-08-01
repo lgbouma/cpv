545811ecaf00253066163279a8b542574f2ee63b
lgbouma <bouma.luke@gmail.com>
2023-07-31 17:45:50 -0700

    ditto

--- drivers/plot_spectrum_windows.py
+++ drivers/plot_spectrum_windows.py
@@ -6,5 +6,11 @@ PLOTDIR = os.path.join(RESULTSDIR, 'spectrum_windows')
 if not os.path.exists(PLOTDIR):
     os.mkdir(PLOTDIR)
 
-rp.plot_spectrum_windows(PLOTDIR)
+#starid = 'TIC146539195'
+
+starids = 'TIC141146667 TIC146539195 TIC264599508 TIC408188366 TIC59129133'
+starids = starids.split(" ")
+
+for starid in starids:
+    rp.plot_spectrum_windows(PLOTDIR, starid, inst='HIRES')
 
