79462e169760c523ecdbe7da189ce32b2bc997cf
lgbouma <bouma.luke@gmail.com>
2023-08-03 14:24:56 -0700

    add calc_sini_guess

---
+++ drivers/calc_sini_guess.py
@@ -0,0 +1,22 @@
+import numpy as np
+from astropy import constants as c, units as u
+
+# tic 89026133 estimate
+Rstar = 1.33*u.Rsun
+P = 11.2*u.hr
+v = (2*np.pi*Rstar / P).to(u.km/u.s)
+vsini = 60 *u.km/u.s
+
+print(v)
+print(vsini)
+print(vsini/v)
+
+# tic 167664935 estimate
+Rstar = 1.42*u.Rsun
+P = 14.05*u.hr
+v = (2*np.pi*Rstar / P).to(u.km/u.s)
+vsini = 100 *u.km/u.s
+
+print(v)
+print(vsini)
+print(vsini/v)
