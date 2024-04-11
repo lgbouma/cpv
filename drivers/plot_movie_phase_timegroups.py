fef7bf3b605a7395793ce700f89fcf0b4fcc0348
lgbouma <bouma.luke@gmail.com>
2024-04-11 15:55:31 -0700

    ditto: add tic 3006

--- drivers/plot_movie_phase_timegroups.py
+++ drivers/plot_movie_phase_timegroups.py
@@ -13,29 +13,41 @@ if not os.path.exists(PLOTDIR): os.mkdir(PLOTDIR)
 def main():
 
     ticids = [
-        "402980664" # the one and only
+        # "402980664" # the one and only
+        "300651846"
     ]
+
+    paramdict = {
+        # period in hr, t0, ylim, sector range
+        '402980664': [18.5611, 1791.12, [-4.8,3], None, 3],  #1791.15 default?
+        '300651846': [8.254, 2170.+12*(8.254/24), [-9.6, 4.9], range(61,70), 5]
+    }
 
     for ticid in ticids:
 
         outdir = os.path.join(PLOTDIR, f'TIC_{ticid}')
         if not os.path.exists(outdir): os.mkdir(outdir)
 
-        for manual_period in [18.5611]:
-            for r in [0]:
-                cp.plot_movie_phase_timegroups(
-                    outdir,
-                    ticid=f'TIC_{ticid}',
-                    lc_cadences='2min',
-                    binsize_phase=0.005,
-                    #t0=1791.15, # NOTE default
-                    t0=1791.12,
-                    #t0=1791.19,
-                    manual_period=manual_period/24,
-                    ylim=[-4.8,3],
-                    showtitle=0,
-                    rasterized=r
-                )
+        manual_period = paramdict[ticid][0]
+        t0 = paramdict[ticid][1]
+        ylim = paramdict[ticid][2]
+        sector_range = paramdict[ticid][3]
+        N_cyclestobin = paramdict[ticid][4]
+
+        for r in [0]:
+            cp.plot_movie_phase_timegroups(
+                outdir,
+                ticid=f'TIC_{ticid}',
+                lc_cadences='2min',
+                binsize_phase=0.005,
+                t0=t0,
+                manual_period=manual_period/24,
+                ylim=ylim,
+                showtitle=0,
+                rasterized=r,
+                sector_range=sector_range,
+                N_cyclestobin=N_cyclestobin
+            )
 
 if __name__ == "__main__":
     main()
