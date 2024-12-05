da8d3295d9dd6a3c14c7ab3a8afb403a5d9a36f6
lgbouma <bouma.luke@gmail.com>
2024-12-05 10:13:18 -0800

    minor

--- tests/test_emission_modelfitter.py
+++ tests/test_emission_modelfitter.py
@@ -16,15 +16,16 @@ from complexrotators.modelfitter import (
 
 # Define specific true parameters
 num_time_points = 50
-K_n_true = [1.8, 3.3]
-phi_n_true = [1.7, 3.4]
-sigma_n_true = [0.3, 0.5]
-A_n_t_true = [1, 0.7]  # Constant amplitude
+K_n_true = [2.1, 2.6, 4]
+phi_n_true = [0.1, 0.1, 3.1]
+sigma_n_true = [0.2, 0.2, 0.2]
+A_n_true = [0.2, 0.2, 0.1]  # Constant amplitude
 period_true = 1.0
+period_true_arr = [1.0]*3
 
 # Generate synthetic data
 xval, yval, flux_arr, err_flux_arr = generate_synthetic_data_specific_params(
-    K_n_true, phi_n_true, sigma_n_true, A_n_t_true,
+    K_n_true, phi_n_true, sigma_n_true, A_n_true,
     num_time_points,
     period_true=period_true,
     num_velocity_points=100,
@@ -39,12 +40,12 @@ flux_arr_masked[:,mask] = np.nan  # Set masked values to NaN
 # fit dataaaa
 
 np.random.seed(42)
-eps = 0.5
-K_n_guess = nparr([1.8, 3.3]) + np.random.normal(0, eps)
-phi_n_guess = nparr([1.7, 3.4]) + np.random.normal(0, eps)
-sigma_n_guess = nparr([0.3, 0.5]) + np.random.normal(0, eps)
-A_n_guess = nparr([1, 0.7]) + np.random.normal(0, eps)  # Constant amplitude
-period_guess = nparr([1.0, 1.0]) + np.random.normal(0, eps)
+eps = 0.01
+K_n_guess = nparr(K_n_true) + np.random.normal(0, eps)
+phi_n_guess = nparr(phi_n_true) + np.random.normal(0, eps)
+sigma_n_guess = nparr(sigma_n_true) + np.random.normal(0, eps)
+A_n_guess = nparr(A_n_true) + np.random.normal(0, eps)  # Constant amplitude
+period_guess = nparr(period_true_arr) + np.random.normal(0, eps)
 guessdict = {
     'K': K_n_guess,
     'phi': phi_n_guess,
@@ -55,7 +56,7 @@ guessdict = {
 
 m = ModelFitter(
     xval, yval, flux_arr_masked, err_flux_arr,
-    modelid='2_gaussians', N_samples=1000, N_cores=2, N_chains=2,
+    modelid='3_gaussians', N_samples=1000, N_cores=2, N_chains=2,
     plotdir=plotdir, overwrite=False, guessdict=guessdict, map_guess_method='handtuned'
 )
 
