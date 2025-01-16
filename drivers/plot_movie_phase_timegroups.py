5895b7c85b1eb3fdd1ce9657d9d95a3ca0581ba0
lgbouma <bouma.luke@gmail.com>
2025-01-16 14:13:21 -0800

    movie: timegroups, add 1411

--- drivers/plot_movie_phase_timegroups.py
+++ drivers/plot_movie_phase_timegroups.py
@@ -13,14 +13,16 @@ if not os.path.exists(PLOTDIR): os.mkdir(PLOTDIR)
 def main():
 
     ticids = [
-        "402980664" # the one and only
+        #"402980664"
         #"300651846"
+        "141146667"
     ]
 
     paramdict = {
-        # period in hr, t0, ylim, sector range
-        '402980664': [18.5611, 1791.12, [-4.8,3], None, 3],  #1791.15 default?
-        '300651846': [8.254, 2170.+12*(8.254/24), [-9.6, 4.9], range(61,70), 5]
+        # period in hr, t0, ylim, sector range, N_cyclestobin, binsize_phase
+        '402980664': [18.5611, 1791.12, [-4.8,3], None, 3, 0.005],  #1791.15 default?
+        '300651846': [8.254, 2170.+12*(8.254/24), [-9.6, 4.9], range(61,70), 5, 0.005],
+        '141146667': [3.930, 2420, [-8, 8], None, 3, 0.01]
     }
 
     for ticid in ticids:
@@ -33,13 +35,14 @@ def main():
         ylim = paramdict[ticid][2]
         sector_range = paramdict[ticid][3]
         N_cyclestobin = paramdict[ticid][4]
+        binsize_phase = paramdict[ticid][5]
 
         for r in [0]:
             cp.plot_movie_phase_timegroups(
                 outdir,
                 ticid=f'TIC_{ticid}',
                 lc_cadences='2min',
-                binsize_phase=0.005,
+                binsize_phase=binsize_phase,
                 t0=t0,
                 manual_period=manual_period/24,
                 ylim=ylim,
@@ -47,7 +50,7 @@ def main():
                 rasterized=r,
                 sector_range=sector_range,
                 N_cyclestobin=N_cyclestobin,
-                style='science_wob',
+                style='science',
                 arial_font=1
             )
 
