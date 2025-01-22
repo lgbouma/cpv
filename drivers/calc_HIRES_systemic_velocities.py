db449706793cdbabf0c52299348f5a33529dbd28
lgbouma <bouma.luke@gmail.com>
2025-01-22 01:50:45 -0300

    update hires systemic RV getter

--- drivers/calc_HIRES_systemic_velocities.py
+++ drivers/calc_HIRES_systemic_velocities.py
@@ -1,43 +1,41 @@
 import os
+from glob import glob
+from os.path import join
 import numpy as np, matplotlib.pyplot as plt, pandas as pd
 from cdips_followup.spectools import get_naive_rv
+from complexrotators.paths import DATADIR, RESULTSDIR
 
-outdir = '/Users/luke/Dropbox/proj/cpv/results/HIRES_systemic_velocities'
+outdir = join(RESULTSDIR, 'HIRES_systemic_velocities')
 if not os.path.exists(outdir): os.mkdir(outdir)
-run_in_parallel = 1
 
-# NOTE: tested in /cdips_followup/tests/test_naive_rv_zeropoint.py
-
-RUNDICT = {
-    # file name, grid teff, %2f grid logg, expected RV (if there is one)
-    'TIC402980664': ['rj520.93.fits', 3300, 5.00, -12.33], # 5 & 11 are off!!  13,14,15 all good
-    'TIC141146667_537.166': ['rj537.166.fits', 3000, 4.50, 0],
-    'TIC141146667_537.167': ['rj537.167.fits', 3000, 4.50, 0],
-    'TIC141146667_500.288': ['rj500.288.fits', 3000, 4.50, 0],
-}
-
-def main():
+def main(RUNDICT, fitsdir, VBROAD, starid, run_in_parallel):
 
     rv_list = []
     drvs = []
     std_rvs = []
+    fitsnames = []
 
     for starname,v in RUNDICT.items():
 
         fitsname, teff, logg, rv_expected = v
+        fitsnames.append(fitsname)
 
         dirstarname = starname
         if "_" in starname:
             dirstarname = starname.split("_")[0]
 
-        spectrum_path = (f'/Users/luke/Dropbox/proj/cdips_followup/data/'
-                         f'spectra/HIRES/{dirstarname}/{fitsname}')
-        synth_path = (f'/Users/luke/local/synthetic_spectra/PHOENIX_MedRes/'
-                      f'lte0{teff:d}-{logg:.2f}-0.0'
-                      f'.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits')
+        spectrum_path = join(fitsdir, fitsname)
+
+        localdir = join(os.path.expanduser('~'), 'local')
+        synth_path = join(
+            localdir,
+            f'synthetic_spectra/PHOENIX_MedRes/'
+            f'lte0{teff:d}-{logg:.2f}-0.0'
+            f'.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits'
+        )
 
         df = get_naive_rv(spectrum_path, synth_path, outdir, make_plot=1,
-                          run_in_parallel=run_in_parallel)
+                          run_in_parallel=run_in_parallel, vbroad=VBROAD)
 
         # recalculate!
         rvs = np.array(df['rv_chisq_kms'])
@@ -49,7 +47,7 @@ def main():
         rchip_good_orders = [0,1,2,3,4,7,8,13,14,15] # default for ZP calc: [0,2,3,4,5,6,11]
 
         # TIC141146667
-        rchip_good_orders = [2,6] # default for ZP calc: [0,2,3,4,5,6,11]
+        rchip_good_orders = [2,6,13] # default for ZP calc: [0,2,3,4,5,6,11]
 
         sel_rvs = rvs[np.array(rchip_good_orders)]
         df['meangoodorder_rv_chisq_minus_bc_kms'] = np.round(np.nanmean(sel_rvs), 4)
@@ -73,6 +71,7 @@ def main():
         drvs.append(drv)
 
     df = pd.DataFrame({
+        'fitsname': fitsnames,
         'starname': list(RUNDICT.keys()),
         'systemic_rv': rv_list,
         'std_rv_stat': std_rvs,
@@ -83,33 +82,33 @@ def main():
         +
         df.std_rv_zp ** 2
     )
+
+    outpath = join(outdir, f'{starid}_systemic_velocities.csv')
+    df.to_csv(outpath, index=False)
+    print(f'Wrote {outpath}')
+
     import IPython; IPython.embed()
-
-    #drvs = np.array(drvs)
-    #std_rvs = np.array(std_rvs)
-
-    #weights = 1.0 / (std_rvs**2)
-    ## calculate zero-point, and its uncertainty, using a weighted sum based on
-    ## each uncertainty (measured via order to order scatter)
-    #wmean = np.sum(drvs * weights) / np.sum(weights)
-    #wstd = np.sqrt(1.0 / np.sum(weights))
-
-    #zeropoint = 1. * wmean  + ZP
-    #zerpoint_unc = 1. * wstd
-
-    #plt.close("all")
-    #fig, ax = plt.subplots()
-    #ax.errorbar(np.arange(len(drvs)), drvs, yerr=std_rvs, ls=":", c='k', markersize=2)
-    #ax.set_xlabel("star index (G8 left, M4.5 right)")
-    #ax.set_ylabel("expected RV - my RV - zeropoint")
-    #title = f'zeropoint = {zeropoint:.3f} +/- {zerpoint_unc:.3f} km/s'
-    #ax.set_title(title)
-    #outpath = os.path.join(outdir, "zeropoint.png")
-    #fig.savefig(outpath, bbox_inches='tight', dpi=300)
-
-    #print(title)
-    #print(f"saved {outpath}")
 
 
 if __name__ == "__main__":
-    main()
+
+    run_in_parallel = 1
+
+    starid = 'TIC141146667'
+    VBROAD = 130
+    fitsdir = join(DATADIR, 'spectra/HIRES/TIC141146667_DEBLAZED')
+    chip = 'r'
+    fitspaths = np.sort(glob(join(fitsdir, f"{chip}j*fits")))
+    RUNDICT = {}
+    for fitspath in fitspaths:
+        ind = os.path.basename(fitspath).rstrip(".fits").lstrip(f"{chip}")
+        key = f'TIC141146667_{ind}'
+        # file name, grid teff, %2f grid logg, expected RV (if there is one)
+        val = [os.path.basename(fitspath), 3000, 4.00, 0]
+        RUNDICT[key] = val
+
+
+    ## file name, grid teff, %2f grid logg, expected RV (if there is one)
+    #RUNDICT = {'TIC402980664': ['rj520.93.fits', 3300, 5.00, -12.33] }
+
+    main(RUNDICT, fitsdir, VBROAD, starid, run_in_parallel)
