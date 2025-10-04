9251c8a75c15fd957109895f3e776a79dd907bb1
lgbouma <bouma.luke@gmail.com>
2025-10-04 09:59:47 -0700

    ditto

--- drivers/plot_movie_sixpanel_specriver.py
+++ drivers/plot_movie_sixpanel_specriver.py
@@ -13,17 +13,33 @@ if not os.path.exists(PLOTDIR): os.mkdir(PLOTDIR)
 
 def main():
 
-    ticids = [
-        "141146667"
-    ]
+    ##########################################
+    # USER CHOICE
+    # ticid = "141146667"
+    ticid = "402980664"
+    datestr = 'j547'
+    ##########################################
 
-    lcdir = ('/Users/luke/Dropbox/proj/cpv/data/photometry/'+
-             'tess/mastDownload/TESS/'+
-             'tess2024030031500-s0075-0000000141146667-0270-s'
+    if ticid == '141146667':
+        lcdir = ('/Users/luke/Dropbox/proj/cpv/data/photometry/'+
+                 'tess/mastDownload/TESS/'+
+                 'tess2024030031500-s0075-0000000141146667-0270-s'
+                )
+        lcpath = join(
+            lcdir, "tess2024030031500-s0075-0000000141146667-0270-s_lc.fits"
+        )
+    elif ticid == '402980664':
+        lcdir = '/Users/luke/Dropbox/proj/cpv/data/photometry/tess'
+        if datestr in ['j546', 'j547']:
+            lcpath = join(
+                lcdir, "tess2024326142117-s0086-0000000402980664-0283-s_lc.fits"
             )
-    lcpath = join(
-        lcdir, "tess2024030031500-s0075-0000000141146667-0270-s_lc.fits"
-    )
+        elif datestr in ['j531', 'j533']:
+            lcpath = join(
+                lcdir, "tess2023341045131-s0073-0000000402980664-0268-s_lc.fits"
+            )
+        else:
+            raise NotImplementedError
 
     linestr = 'Hα' # "Hγ"
 
@@ -33,13 +49,21 @@ def main():
                       3339.9326,
                       [-10, 6], 75,
                       {'Hα':[0.7, 3.6], 'Hγ': [0.7, 7], 'CaH': [0.7, 10]}, 15],
+        '402980664': [18.5611,
+                      1791.12,
+                      [-4.8,3], 73,
+                      {'Hα':[0.7, 2], 'Hγ': [0.7, 7], 'CaH': [0.7, 10]}, 10],
     }
     cb_tickd = {
         '141146667': {
             'Hα': [1,2], 'Hγ': None, 'CaH': None
         },
+        '402980664': {
+            'Hα': [1,2], 'Hγ': None, 'CaH': None
+        }
     }
 
+    ticids = [ticid]
     for ticid in ticids:
 
         manual_period = paramdict[ticid][0]
@@ -49,9 +73,9 @@ def main():
         lamylim = paramdict[ticid][4][linestr]
         dlambda = paramdict[ticid][5]
 
-        style = 'science' # "science_wob" or "science"
+        style = 'science_wob' # "science_wob" or "science"
 
-        outdir = os.path.join(PLOTDIR, f'TIC_{ticid}_{style}')
+        outdir = os.path.join(PLOTDIR, f'TIC_{ticid}_{datestr}_{style}')
         if not os.path.exists(outdir): os.mkdir(outdir)
 
         cp.plot_movie_sixpanel_specriver(
@@ -75,7 +99,8 @@ def main():
             arial_font=1,
             dlambda=dlambda,
             lognorm=0,
-            showsinusoid=1
+            showsinusoid=0,
+            datestr=datestr
         )
 
 if __name__ == "__main__":
