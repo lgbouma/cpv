ce0ed999bc3de7a1c47c26ed3af767e4fa3ce894
lgbouma <bouma.luke@gmail.com>
2025-10-17 09:30:02 -0700

    drivers: ditto

--- drivers/plot_movie_sixpanel_specriver.py
+++ drivers/plot_movie_sixpanel_specriver.py
@@ -15,9 +15,16 @@ def main():
 
     ##########################################
     # USER CHOICE
+
+    # TIC 141146667: j531, j533
     # ticid = "141146667"
-    ticid = "402980664"
-    datestr = 'j547'
+
+    # LP 12-5602: j546, j547
+    # ticid = "402980664"
+    # datestr = 'j547'
+
+    ticid = '300651846'
+    datestr = '20250102'
     ##########################################
 
     if ticid == '141146667':
@@ -40,8 +47,17 @@ def main():
             )
         else:
             raise NotImplementedError
+    elif ticid == '300651846':
+        lcdir = '/Users/luke/Dropbox/proj/cpv/data/photometry/tess'
+        lcpath = join(
+            lcdir, 'tess2024353092137-s0087-0000000300651846-0284-s_lc.fits'
+        )
 
     linestr = 'Hα' # "Hγ"
+    dlambda = 15
+
+    linestr = 'Hβ' # "Hγ"
+    dlambda = 15
 
     paramdict = {
         # period in hr, t0, flux ylim, sector number, lambdaylim, dlambda
@@ -53,13 +69,21 @@ def main():
                       1791.12,
                       [-4.8,3], 73,
                       {'Hα':[0.7, 2], 'Hγ': [0.7, 7], 'CaH': [0.7, 10]}, 10],
+        '300651846': [0.3439207894624283*24,
+                      2460693.243837336-2457000,
+                      [-8,5], 87,
+                      {'Hα':[0.8, 2.5], 'Hβ': [0.7, 6.7], 'Hγ': [0.7, 10]}, dlambda],
     }
+
     cb_tickd = {
         '141146667': {
             'Hα': [1,2], 'Hγ': None, 'CaH': None
         },
         '402980664': {
             'Hα': [1,2], 'Hγ': None, 'CaH': None
+        },
+        '300651846': {
+            'Hα': [1,1.5], 'Hβ': None, 'Hγ': None, 'CaH': None
         }
     }
 
